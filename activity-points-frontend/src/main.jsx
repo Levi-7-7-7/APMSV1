e8e845850d2e3f62065239ceefee290a87752bd8
Lijo Thomas <lijothomas7777777@gmail.com>
2026-07-19 00:26:54 +0530

    dark theme added

--- activity-points-frontend/src/main.jsx
+++ activity-points-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import './css/theme.css';
 import './css/CertModal.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
